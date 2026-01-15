cd9f134bd58f8dea0b65ae5ae68d177b189bbbb1
Caroline-Cuff <caroline_cuff28@milton.edu>
2026-01-15 14:13:24 -0500

    more updates + fixed 60+ errors :)

--- src/BasicGameApp.java
+++ src/BasicGameApp.java
@@ -12,6 +12,7 @@
 //import java.awt.Canvas;
 
 //Graphics Libraries
+
 import java.awt.Graphics2D;
 import java.awt.image.BufferStrategy;
 import java.awt.*;
@@ -146,61 +147,63 @@ public class BasicGameApp implements Runnable {
 //
 //        }
 
+
+
+//Pauses or sleeps the computer for the amount specified in milliseconds
+public void pause(int time) {
+    //sleep
+    try {
+        Thread.sleep(time);
+    } catch (InterruptedException e) {
     }
+}
 
-   //Pauses or sleeps the computer for the amount specified in milliseconds
-   public void pause(int time ){
-   		//sleep
-			try {
-				Thread.sleep(time);
-			} catch (InterruptedException e) {}
+//Graphics setup method
+private void setUpGraphics() {
+    frame = new JFrame("Application Template");   //Create the program window or frame.  Names it.
 
-   //Graphics setup method
-   private void setUpGraphics() {
-      frame = new JFrame("Application Template");   //Create the program window or frame.  Names it.
-   
-      panel = (JPanel) frame.getContentPane();  //sets up a JPanel which is what goes in the frame
-      panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));  //sizes the JPanel
-      panel.setLayout(null);   //set the layout
-   
-      // creates a canvas which is a blank rectangular area of the screen onto which the application can draw
-      // and trap input events (Mouse and Keyboard events)
-      canvas = new Canvas();  
-      canvas.setBounds(0, 0, WIDTH, HEIGHT);
-      canvas.setIgnoreRepaint(true);
-   
-      panel.add(canvas);  // adds the canvas to the panel.
-   
-      // frame operations
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //makes the frame close and exit nicely
-      frame.pack();  //adjusts the frame and its contents so the sizes are at their default or larger
-      frame.setResizable(false);   //makes it so the frame cannot be resized
-      frame.setVisible(true);      //IMPORTANT!!!  if the frame is not set to visible it will not appear on the screen!
-      
-      // sets up things so the screen displays images nicely.
-      canvas.createBufferStrategy(2);
-      bufferStrategy = canvas.getBufferStrategy();
-      canvas.requestFocus();
-      System.out.println("DONE graphic setup");
-   
-   }
+    panel = (JPanel) frame.getContentPane();  //sets up a JPanel which is what goes in the frame
+    panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));  //sizes the JPanel
+    panel.setLayout(null);   //set the layout
+
+    // creates a canvas which is a blank rectangular area of the screen onto which the application can draw
+    // and trap input events (Mouse and Keyboard events)
+    canvas = new Canvas();
+    canvas.setBounds(0, 0, WIDTH, HEIGHT);
+    canvas.setIgnoreRepaint(true);
+
+    panel.add(canvas);  // adds the canvas to the panel.
+
+    // frame operations
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //makes the frame close and exit nicely
+    frame.pack();  //adjusts the frame and its contents so the sizes are at their default or larger
+    frame.setResizable(false);   //makes it so the frame cannot be resized
+    frame.setVisible(true);      //IMPORTANT!!!  if the frame is not set to visible it will not appear on the screen!
+
+    // sets up things so the screen displays images nicely.
+    canvas.createBufferStrategy(2);
+    bufferStrategy = canvas.getBufferStrategy();
+    canvas.requestFocus();
+    System.out.println("DONE graphic setup");
+
+}
 
 
-	//paints things on the screen using bufferStrategy
-	private void render() {
-		Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
-		g.clearRect(0, 0, WIDTH, HEIGHT);
+//paints things on the screen using bufferStrategy
+private void render() {
+    Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
+    g.clearRect(0, 0, WIDTH, HEIGHT);
 
-      //draw the image of the astronaut
-        g.drawImage(backgroundpic,0,0,WIDTH, HEIGHT,null);
-        g.drawImage(teaPic, earlg.xpos, earlg.ypos, earlg.width, earlg.height, null);
-        g.drawImage(queenPic, liz.xpos, liz.ypos,liz.width, liz.height,null);
-        g.drawImage(crownpic,coro.xpos, coro.ypos, coro.width,coro.height,null);
-        g.drawImage(telepic,telebo.xpos, telebo.ypos, telebo.width, telebo.height, null);
+    //draw the image of the astronaut
+    g.drawImage(backgroundpic, 0, 0, WIDTH, HEIGHT, null);
+    g.drawImage(teaPic, earlg.xpos, earlg.ypos, earlg.width, earlg.height, null);
+    g.drawImage(queenPic, liz.xpos, liz.ypos, liz.width, liz.height, null);
+    g.drawImage(crownpic, coro.xpos, coro.ypos, coro.width, coro.height, null);
+    g.drawImage(telepic, telebo.xpos, telebo.ypos, telebo.width, telebo.height, null);
 
 
-		g.dispose();
+    g.dispose();
 
-		bufferStrategy.show();
-	}
+    bufferStrategy.show();
+}
 }
